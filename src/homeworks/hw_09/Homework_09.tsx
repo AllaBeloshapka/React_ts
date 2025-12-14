7b94d797cf49216a7cff62a0660444484464ee4d
AllaBeloshapka <allabeloshapka4@gmail.com>
2025-12-14 17:05:43 +0100

    add Lesson_10 & add hw_09

--- src/homeworks/hw_09/Homework_09.tsx
+++ src/homeworks/hw_09/Homework_09.tsx
@@ -1,68 +1,64 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import {v4} from 'uuid'
 
-import { TASK_FORM_VALUES } from "./types";
-import { type FormEvent, type ChangeEvent, useState } from "react";
 import Button from "components/Button/Button";
-import "./styles";
 import Input from "components/Input/Input";
-import { Form, NameBox, Notes, TaskContainer } from "./styles";
-import { v4 } from "uuid";
 
+import {
+  PageWrapper,
+  NoteForm,
+  ButtonControl,
+  Notes,
+  NoteItem,
+  NoteTitle,
+} from "./styles";
 
 function Homework_09() {
-  //Шаг 1: Создаем стейты для контроля HTML элеметов Input
-  const [inputTask, setInput] = useState<string>("");
+  const [note, setNote] = useState<string>("");
+  const [savedNotes, setSavedNotes] = useState<string[]>([]);
 
-  //Шаг 2: Создаем обработчик изменений для Input, ловим событие onChange
-  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
-    console.log(event.target.value);
+  const onNoteChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNote(event.target.value);
   };
 
-  //Шаг 5: создаём массив для хранения задач
-  const [tasks, setTasks] = useState<string[]>([]);
-
-
-  //Шаг 3: Создаем функцию addTask для переноса введённого текста в блок Notes по событию onSubmit формы
-  const addTask = (event: FormEvent<HTMLFormElement>) => {
+  const onSaveNote = (event: FormEvent) => {
     event.preventDefault();
-
-    //Шаг 6: Создаём фунцию добавления задачи в массив задач
-    if (inputTask.trim() === "") 
-      return;
-
-    //Шаг 8: Добавляем новую задачу в стейт
-    setTasks((prev) => [...prev, inputTask]);
-
-    // Очищаем поле ввода
-    setInput("");
+    // prevValue === savedNotes в момент вызова функции setSavedNotes
+    setSavedNotes((prevValue: string[]) => {
+      console.log(prevValue);
+      return [...prevValue, note];
+    });
   };
+
+  console.log(savedNotes);
+  // Допустим savedNotes = ["Coocking", "Shopping"]
+  // Наша задача вернуть новый массив notesList который будет иметь вид(условно говоря) = [<NoteItem>Coocking</NoteItem>, <NoteItem>Shopping</NoteItem>]
+  const notesList = savedNotes.map((value: string) => {
+    return <NoteItem key={v4()}>{value}</NoteItem>
+  });
 
   return (
-    <TaskContainer>
-      <NameBox>Plan for the day</NameBox>
-
-      <Notes>
-        {tasks.map((task: string, index: number) => (
-          <div key={v4()}>{task}</div>
-        ))}
-      </Notes>
-
-      <Form onSubmit={addTask}>
+    <PageWrapper>
+      <NoteForm onSubmit={onSaveNote}>
         <Input
-          id={"input-task-id"}
-          name={TASK_FORM_VALUES.INPUT}
-          placeholder={"Enter the task"}
-          label={""}
-          //Шаг 4: Привязываем стейты и обработчики к инпуту
-          value={inputTask}
-          onChange={onChangeInput}
+          id="todo-input"
+          name="todo"
+          placeholder="Enter your note"
+          label="Note"
+          value={note}
+          onChange={onNoteChange}
         />
-        <Button name={"Add a task"} type="submit" />
-        {/* создаёмкнопку для удаления задачи из списка */}
-        <Button name={"Delete"} type={undefined}></Button>
-      </Form>
-    </TaskContainer>
+        <ButtonControl>
+          <Button name="Add" type="submit" />
+        </ButtonControl>
+      </NoteForm>
+      <Notes>
+        <NoteTitle>Your notes</NoteTitle>
+        {notesList}
+      </Notes>
+    </PageWrapper>
   );
 }
 
 export default Homework_09;
+
--- src/homeworks/hw_09/styles.ts
+++ src/homeworks/hw_09/styles.ts
@@ -1,45 +1,49 @@
 import styled from "@emotion/styled";
-// Styles for the homework 09
 
-export const TaskContainer = styled.div`
+export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
-  gap: 20px;
-  padding: 20px;
-  background-color: #91b192ff;
-  width: 650px;
-  height: 100%;
-  border-radius: 15px;
+  flex: 1;
+  padding: 100px;
+  gap: 30px;
 `;
 
-export const NameBox = styled.h1`
-  font-size: 50px;
+export const NoteForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  gap: 10px;
+  width: 600px;
+  height: fit-content;
+`;
+
+export const ButtonControl = styled.div`
+  width: 200px;
+`;
+
+export const Notes = styled.ol`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 400px;
+  height: fit-content;
+  border: 2px solid black;
+  padding: 30px;
+  border-radius: 6px;
+`;
+
+export const NoteTitle = styled.p`
+  font-size: 24px;
   font-weight: bold;
-  margin-bottom: 10px;
-  text-align: center;
-  color: #013501ff;
+  color: purple;
 `;
 
-export const Notes = styled.div`
-  width: 600px;
-  height: 400px;
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-  padding: 15px;
-  border: 1px solid #003212ff;
-  background-color: #c1dbc7ff;
-  border-radius: 10px;
-`;
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  width: 600px;
+export const NoteItem = styled.li`
+  font-size: 20px;
+  font-weight: bold;
 `;
 
-export const TaskTitle = styled.h2``;
 
 
